602874e59bd9e8bccd8f36337e408fbb59da170c
Aliaksandr <aliaksandr.zuyeuski@gmail.com>
2023-01-28 20:29:38 +0100

    add coment

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,6 +8,8 @@ function solveEquation(a, b, c) {
   } else if (discriminant > 0) {
     arr.push(((b * -1) + Math.sqrt(discriminant)) / (2 * a));
     arr.push((b * -1 - Math.sqrt(discriminant)) / (2 * a));
+
+//hello
   }
 
   return arr;
@@ -15,14 +17,14 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let resultCreditSum = 0;
-
+g
   if (!typeof (percent / 1).isNaN || !typeof (contribution / 1).isNaN || !typeof (amount / 1).isNaN || !typeof (countMonths / 1).isNaN) {
     return null;
   } else {
 
   const monthPrecent = percent / 12 / 100;
 
-  
+
 
 
   }
